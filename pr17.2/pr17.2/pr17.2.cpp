96d4b9126c68c6ffc9b2252f028a2e2e41fd22a7
AntonBoev14 <antonboev421@gmail.com>
2025-03-17 14:36:35 +0300

    Add files via upload

---
+++ pr17.2/pr17.2/pr17.2.cpp
@@ -0,0 +1,47 @@
+﻿//Дано 2 файла вещественных чисел а1.txt и а2.txt.Найти сумму положительных элементов в двух
+//файлов.Вывести в консоли данные из файлов и результат.
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+void generateFiles();
+{
+	ofstream fout1("a1.txt");
+	ofstream fout2("a2.txt");
+
+	srand(time(0));
+	for (int i = 0; i < 10; ++i) {
+		n = rand() % 201 - 100;
+	}
+	
+	srand(time(0));
+	for (int i = 0; i < 10; ++i) {
+		m = rand() % 201 - 100;
+	}
+	fout.close();
+	fout.close();
+}
+double numberspositivies(const string& filename)
+{
+	
+}
+int main()
+{
+	setlocale(0, "ru");
+
+	ifstream fin("a1.txt");
+	if (!fin.is_open())
+	{
+		cout << "ошибка открытия файла! " << endl;
+	}
+	else
+	{
+		cout << choto << endl;
+	}
+
+	cout << choto << endl;
+
+	system("pause");
+	return 0;
+}
+
